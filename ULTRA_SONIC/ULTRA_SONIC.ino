2d789b266a4d3592abca2f5aa381b1b65d6df6c8
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-07-09 17:45:14 +0200

    ultra sonic

--- ULTRA_SONIC/ULTRA_SONIC.ino
+++ ULTRA_SONIC/ULTRA_SONIC.ino
@@ -1,27 +1,54 @@
+const int trigPin = 16;
 
-long duration;
-float distance;
-const int trigPin = 13;
-const int echoPin = 12;
-void setup() {
+const int echoPin = 5;
+
+long duration, distance;
+
+void setup(){
+
+  // initialize serial communication:
+
   Serial.begin(9600);
-  // put your setup code here, to run once:
-  pinMode(trigPin,OUTPUT);
-  pinMode(echoPin,INPUT);
-  
+
+  pinMode(trigPin, OUTPUT);
+
+  pinMode(echoPin, INPUT);
 
 }
 
-void loop() {
-  digitalWrite(trigPin,LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin,LOW);
+ 
 
-  duration = pulseIn(echoPin,HIGH);
-  distance = duration/58.2;
-  Serial.println(distance);
-  // put your main code here, to run repeatedly:
+
+
+void loop(){
+
+  digitalWrite(trigPin, LOW);
+
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+
+  delayMicroseconds(10);
+
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+
+  distance = (duration / 2) / 29.1;
+
+ 
+
+  if (distance >= 200 || distance <= 0){
+    Serial.println("Out of range");
+   }
+
+   else {
+     Serial.print(distance);
+
+     Serial.println(" cm");
+
+   }
+
+   delay(500);
 
 }
